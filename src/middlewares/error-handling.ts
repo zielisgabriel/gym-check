f724f95da2ced2658108433a040a9f47c0635bc9
zielisgabriel <josegabrielas0586@gmail.com>
2025-01-29 12:49:28 -0300

    error-handling middleware added

---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express'
+import { AppError } from '@/utils/AppError'
+
+export function errorHandling(error: any, req: Request, res: Response, _: NextFunction){
+    if(error instanceof AppError){
+        res.status(error.statusCode).json({ message: error.message })
+        return
+    }
+
+    res.status(500).json({ message: error.message })
+    return
+}
