5c7e119eb94060ebdbe0a6c16c508de9d9e2b569
Gabriel <josegabrielas0586@gmail.com>
2025-01-29 17:46:28 -0300

    create user route added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express from 'express'
+import { routes } from './routes'
+import { errorHandling } from './middlewares/error-handling'
 
 const app = express()
 app.use(express.json())
+
+app.use(routes)
+
+app.use(errorHandling)
 
 export { app }
