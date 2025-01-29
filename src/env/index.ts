201b0a9fef7aa1fa52e2eada37007abb3ec59e70
zielisgabriel <josegabrielas0586@gmail.com>
2025-01-29 12:22:12 -0300

    validation environment variables

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,16 @@ import z from 'zod'
 
 const envSchema = z.object({
     PORT: z.coerce.number().default(3333),
-    HOST: z.coerce.string().default('0.0.0.0')
+    HOST: z.coerce.string().default('0.0.0.0'),
+    NODE_DEV: z.enum(['dev', 'test', 'production']).default('dev'),
 })
 
-export const ENV = envSchema.parse(process.env)
+const _env = envSchema.safeParse(process.env)
+
+if(_env.success === false){
+    console.error('Invalid environment variables', _env.error.format())
+
+    throw new Error('Invalid environment variables.')
+}
+
+export const ENV = _env.data
