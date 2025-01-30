c7df9e0b3686a4546e82e98c7b9ca57c63e93449
Gabriel <josegabrielas0586@gmail.com>
2025-01-30 10:46:30 -0300

    logic in database folder of prisma added

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,3 +1,6 @@
+import { ENV } from '@/env'
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient({
+    log: ENV.NODE_DEV === 'dev' ? ['query'] : []
+})
