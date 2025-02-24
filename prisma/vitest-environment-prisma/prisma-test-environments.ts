4b16b8244bdc7a493e84b87a546badd0248acd45
Gabriel <josegabrielas0586@gmail.com>
2025-02-24 12:13:00 -0300

    update on test environments settings

--- prisma/vitest-environment-prisma/prisma-test-environments.ts
+++ prisma/vitest-environment-prisma/prisma-test-environments.ts
@@ -1,15 +1,37 @@
-import { Environment } from "vitest";
+import { Environment } from "vitest"
 
 export default <Environment>{
-    name: "prisma",
-    transformMode: "ssr",
-    async setup(){
-        console.log("Execution")
+  name: 'prisma',
+  transformMode: 'ssr',
+  setup(){
+    console.log('Setup console')
 
-        return{
-            async teardown(){
-                console.log("Teardown")
-            }
-        }
+    return{
+      teardown(){
+        console.log('Teardown')
+      }
     }
+  }
 }
+
+
+
+
+
+// import { Environment } from "vitest/environments";
+
+// console.log('Custom environment loaded');
+
+// export default {
+//     name: 'prisma',
+//     transformMode: 'ssr',
+//     async setup() {
+//       console.error('Hello from setup');
+      
+//       return {
+//         async teardown() {
+//           console.error('Teardown from prisma environment');
+//         }
+//       };
+//     }
+//   } as Environment;
